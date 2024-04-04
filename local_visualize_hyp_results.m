23d7f0e98913fce922a04603172f76f94af7906e
zanderso13 <32366377+zanderso13@users.noreply.github.com>
2024-04-04 12:03:46 -0500

    Update local_visualize_hyp_results.m

--- local_visualize_hyp_results.m
+++ local_visualize_hyp_results.m
@@ -33,12 +33,12 @@ if visualize_pls_results == 1
         totPCT(i,:,2) = uPCTVAR(2,:);
         totPCT(i,:,4) = transformPCTVAR{i}(2,:);
 
-        loadings_to_visualize(:,:,i) = reshape(transformXL{i}(:,1),[828,71]);
+        %loadings_to_visualize(:,:,i) = reshape(transformXL{i}(:,1),[828,71]);
     end
 
     % visualize MSE
     % no differences in MSE. Distributions look super similar
-    figure(); histogram(allMSE(:,1)); hold on; histogram(allMSE(:,3)); hold on; xline(allMSE(1,2),'LineWidth', 5); hold on; histogram(allMSE(:,4));
+    figure(); histogram(allMSE(:,1)); hold on; xline(allMSE(1,2),'LineWidth', 5); hold on; histogram(allMSE(:,4));
     figure(); histogram(allMSE(:,1)); hold on; xline(allMSE(1,2),'LineWidth', 5); hold on; histogram(allMSE(:,4));
     p_aligned_pls_mse = sum(allMSE(:,1)>allMSE(1,2))./100; % 
     p_transforms_pls_mse = sum(allMSE(:,4)>allMSE(1,2))./100;
@@ -46,16 +46,21 @@ if visualize_pls_results == 1
     % visualize pct variance covered
     % unaligned data is no better than the null dist. Aligned data is WAY
     % better than both in terms of percent variance accounted for
-    pctall = figure(); histogram(allPCT(:,1)); hold on; histogram(allPCT(:,3)); hold on; xline(allPCT(1,2),'LineWidth', 5); hold on; histogram(allPCT(:,4));
+    pctall = figure(); histogram(allPCT(:,1)); hold on; xline(allPCT(1,2),'LineWidth', 5); hold on; histogram(allPCT(:,4)); 
     pcthyp = figure(); histogram(allPCT(:,1)); hold on; xline(allPCT(1,2),'LineWidth', 5); hold on; histogram(allPCT(:,4));
     savefig(pctall,'pctall.fig'); savefig(pcthyp,'pct_hyperalignment.fig');
     p_aligned_pls_pct = sum(allPCT(:,1)<allPCT(1,2))./100; % p < 0.01
     p_transform_pls_pct = sum(allPCT(:,4)<allPCT(1,2))./100; % p < 0.01
     
-    brainfile = filenames('hypatl*mat');
-    load(brainfile{1})
+    
+    load('avg_z_and_brain.mat')
 
-    figure(); montage(hypatl)
+    figure(); montage(hypatl_avg)
+    figure(); montage(hypatl_std)
+
+    hypatl_avg.fullpath = '/Users/zacharyanderson/Documents/ACNlab/dissertation_final/rest_shortcuts/inflammation/hypatl_avg.nii';
+    write(hypatl_avg)
+
      
     
 
